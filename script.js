746387c615d8c7ae970ecce41c6dd02b9d40894e
Abiola <mujeebabiola898@gmail.com>
2023-11-22 19:59:55 +0100

    trying show more function

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
-const showMore = document.querySelector('.show-more-button');
-const content = document.querySelector('.hidden');
-
-showMore.addEventListener('click', () => {
-    content.classList.toggle('.show');
-    // content.classList.toggle('.visible');
-});
+function toggleContent() {
+    const hiddenContent = document.querySelector(".hidden");
+    hiddenContent.classList.toggle("show");
+}
